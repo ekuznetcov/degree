d0c813c610ed77af9ff92323fb3367a9e825e83c
Егор Колганов <egor57k@MacBook-Air-Egor.local>
2022-03-22 23:10:06 +0300

    rewrite formula.go

--- formula/formula.go
+++ formula/formula.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"C"
 	"context"
 	"fmt"
 	_ "github.com/lib/pq"
@@ -11,10 +12,84 @@ import (
 	"time"
 )
 
+type Stats struct {
+	Max model.Value
+	Min model.Value
+	Avg model.Value
+}
+
+type Domain struct {
+	Name  string
+	Stats Stats
+}
+
 //getNodeAggMetrics возвращает агрегированные метрики(min, max, mean) для узлаа
 //hostname string - сетевое имя узла, с к оторого собирались метрики
 //duration Duration - структура содержащая начала и конец выполнения задания
-func getNodeAggMetrics(hostname string, queryStr string, duration v1.Range) (model.Value, error) {
+//export getNodeAggMetrics
+func getNodeAggMetrics(serverAddr string, hostList []string, timeStart time.Time, timeEnd time.Time) (model.Value, error) {
+	//формирование запроса
+	domains := [5]string{"psys", "package", "dram", "uncore", "core"}
+	stats := [3]string{"min", "max", "avg"}
+	hostsReg := ""
+	for _, host := range hostList {
+		hostsReg += host + "|"
+	}
+	duration := timeEnd.Sub(timeStart).Round(time.Second).String()
+
+	var query string
+	for _, domain := range domains {
+		for _, stat := range stats {
+			query = fmt.Sprintf("max without (instance) (%s_over_time(irate(node_rapl_%s_joules_total{instance=~'%s'}[2s])[%s:1s]))", stat, domain, hostsReg, duration)
+			fmt.Println(query)
+
+			//выполнение запроса
+			client, err := api.NewClient(api.Config{
+				Address: serverAddr,
+			})
+			if err != nil {
+				fmt.Printf("Error creating client: %b\n", err)
+				os.Exit(1)
+			}
+			v1api := v1.NewAPI(client)
+			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+			defer cancel()
+
+			result, warnings, err := v1api.Query(ctx, query, timeStart)
+			if err != nil {
+				fmt.Printf("Error querying Prometheus: %v\n", err)
+				os.Exit(1)
+			}
+			if len(warnings) > 0 {
+				fmt.Printf("Warnings: %v\n", warnings)
+			}
+			fmt.Printf("Result:\n%v\n", result)
+		}
+	}
+	return nil, nil
+}
+
+//writeToPostgres пишет агрегированные метрики для запущенного задания в Postgres
+//nodeID int64 - id узла кластера в базе данных
+//runID int64 - id запуска задания
+//metrics AggMetrics - агрегированные значения метрик (max,min,mean)
+//duration Duration - структура содержащая начала и конец выполнения задания
+//func writeToPostgres(nodeID int64, runID int64, metrics AggMetrics, duration Duration) {
+//
+//}
+
+func main() {
+	serverAddr := "http://localhost:9090"
+	hostsList := []string{"localhost:9100"}
+
+	getNodeAggMetrics(serverAddr, hostsList, time.Now().Add(-time.Hour), time.Now())
+}
+
+/*stats := [3]string{"min", "max", "avg"}
+hostname := "http://localhost:9090"
+for _, stat := range stats {
+	query := fmt.Sprintf("%s_over_time(irate(node_cpu_seconds_total{cpu='0', mode=\"iowait\"}\n[30s])[1h:15s])", stat)
+	fmt.Println(query)
 	client, err := api.NewClient(api.Config{
 		Address: hostname,
 	})
@@ -28,7 +103,7 @@ func getNodeAggMetrics(hostname string, queryStr string, duration v1.Range) (mod
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	result, warnings, err := v1api.QueryRange(ctx, queryStr, duration)
+	result, warnings, err := v1api.Query(ctx, query, time.Now().Add(-time.Hour))
 	if err != nil {
 		fmt.Printf("Error querying Prometheus: %v\n", err)
 		os.Exit(1)
@@ -37,33 +112,4 @@ func getNodeAggMetrics(hostname string, queryStr string, duration v1.Range) (mod
 		fmt.Printf("Warnings: %v\n", warnings)
 	}
 	fmt.Printf("Result:\n%v\n", result)
-
-	return result, nil
-}
-
-//writeToPostgres пишет агрегированные метрики для запущенного задания в Postgres
-//nodeID int64 - id узла кластера в базе данных
-//runID int64 - id запуска задания
-//metrics AggMetrics - агрегированные значения метрик (max,min,mean)
-//duration Duration - структура содержащая начала и конец выполнения задания
-//func writeToPostgres(nodeID int64, runID int64, metrics AggMetrics, duration Duration) {
-//
-//}
-
-func main() {
-	domains := [5]string{"psys", "package", "dram", "pp0", "pp1"}
-	stats := [3]string{"min", "max", "avg"}
-	hostname := "http://localhost:9090"
-	for _, domain := range domains {
-		for _, stat := range stats {
-			query := fmt.Sprintf("%s(rate(node_cpu_energy_%s[15s]))", stat, domain)
-			fmt.Println(query)
-			duration := v1.Range{
-				Start: time.Now().Add(-time.Minute * 30),
-				End:   time.Now(),
-				Step:  time.Minute,
-			}
-			getNodeAggMetrics(hostname, query, duration)
-		}
-	}
-}
+}*/
